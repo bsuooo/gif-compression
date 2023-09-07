9ae17cae0d183813e3591b39ff9ae95c26b0304c
xuheng <ob12564@163.com>
2023-09-07 14:27:15 +0800

    perf

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Post,
   Get,
+  Res,
   UseInterceptors,
   UploadedFile,
   ParseIntPipe,
@@ -10,7 +11,9 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { existsSync } from 'fs';
+import { Response } from 'express';
+import { existsSync, unlinkSync } from 'fs';
+import * as sharp from 'sharp';
 
 @Controller()
 export class AppController {
@@ -27,13 +30,33 @@ export class AppController {
   }
 
   @Get('compression')
-  compression(
+  async compression(
     @Query('path') path: string,
     @Query('color', ParseIntPipe) color: number,
+    @Query('level') level: number,
+    @Res() res: Response,
   ) {
     if (!existsSync(path)) {
       throw new BadRequestException('没有查找到文件, 请重新上传');
     }
-    console.log(path, color);
+
+    const numberLevel = Number(level);
+
+    if (numberLevel > 1 || numberLevel < 0) {
+      throw new BadRequestException('级别在0.1 - 1.0 之间!');
+    }
+
+    const data = await sharp(path, {
+      animated: true,
+      limitInputPixels: false,
+    })
+      .gif({ colours: color, dither: numberLevel })
+      .toBuffer();
+    // 压缩完后删除文件
+    // sharp.cache({ files: 0 });
+    // unlinkSync(path);
+
+    res.set('Content-Disposition', `attachment; filename="dest.gif"`);
+    res.send(data);
   }
 }
--- web/src/App.js
+++ web/src/App.js
@@ -1,36 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InboxOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
+import { message, Upload, Form, Input, Button } from 'antd';
+import axios from 'axios';
+
 const { Dragger } = Upload;
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'http://localhost:3001/upload',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
+
+const App = () => {
+  const [form] = Form.useForm();
+  const [filePath, setFilePath] = useState('');
+  const [fileName, setFileName] = useState('');
+
+  const compress = async (values) => {
+    try {
+      const res = await axios.get('http://localhost:3001/compression', {
+        params: {
+          color: values.color || 256,
+          level: values.level || 9,
+          path: filePath
+        },
+        responseType: 'arraybuffer'
+      });
+
+      const blob = new Blob([res.data], { type: 'image/jpeg' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = fileName;
+      link.click();
+
+      message.success('压缩成功');
+    } catch (error) {
+      message.error(`压缩失败,${error}`);
     }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
+  };
+
+  const props = {
+    name: 'file',
+    action: 'http://localhost:3001/upload',
+    maxCount: 1,
+    onChange(info) {
+      const { status } = info.file;
+      if (status === 'done') {
+        setFilePath(info.file.response);
+        setFileName(info.file.name);
+        message.success(`${info.file.name} 文件上传成功`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} 文件上传失败`);
+      }
     }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
-};
-const App = () => (
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
-      <InboxOutlined />
-    </p>
-    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-    <p className="ant-upload-hint">
-      Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-      banned files.
-    </p>
-  </Dragger>
-);
+  };
+
+  return <div>
+    <Form style={{ width: 500, margin: '50px auto' }} form={form} onFinish={compress}>
+      <Form.Item
+        label="颜色数量"
+        name="color"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="压缩级别"
+        name="level"
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item>
+        <Dragger {...props}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">点击或拖拽文件到这个区域来上传</p>
+        </Dragger>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">压缩</Button>
+      </Form.Item>
+    </Form>
+  </div >
+}
+
 export default App;
